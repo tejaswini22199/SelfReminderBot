0c409f6b5156046e12c2ded8538fa2a4f2be7d73
tejaswini22199 <tejaswini22199@gmail.com>
2021-03-07 01:37:11 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+### Discord Reminder
+
+Features:
+ * If you are participating in a hackathon or if you have any contest. It is likely that you forget after you register and sometimes you may miss it also. You make a Todo List but you dont see it . But due to pandamic we spend so much time in discord to chat with others or participating in anu competition. So I thought of building a discord bot, Which will remind you about your contests, hackathons, to take rest, to drink water, to have lunch and dinner. Other features include
+
+ Technology Stack used:
+ * Discord.js
+ * CockroachdB data base
+ 
+
+## Inspiration
+If you are participating in a hackathon or if you have any contest or if you forgot to take tablets on time. It is likely that you forget after you register and sometimes you may miss it also. You make a Todo List but you dont see it . But due to pandamic we spend so much time in discord to chat with others or participating in any competition. So I thought of building a discord bot, Which will remind you about your contests, hackathons, to take rest, to drink water, to have lunch and dinner.
+## What it does
+Whenever user uses this bot in their server, you need to mention the bot about your contest/hackathon by using following command $remindContest. Also you need to add some arguments like $remindContest `Contestname` `ContestLink` `ContestTime`. It will remind you at that time. It will automatically send you message in the timestamp difference of 1 hour to drink water. It will send you about your lunch at 12:30 dinner at 7:30 breakfast at 8:00.
+And if you want a fun task to do you can ask the bot using $funTask, Gives you a fun task.
+
+## How we built it
+Using discord.js which is a powerful library containing many functions. And Cockroach db , a powerful distrubuted Data base, the data is stored.
+## Challenges we ran into
+I always wanted to build a bot but don;t know about discord.js. Now when I implemented I got into some challen
+## Accomplishments that we're proud of
+
+## What we learned
+Learned so much about Java Script and CoakroachdB. And why coakraoch db is special and superior over our traditional data bases as well as noSQL data bases. And Java script event handling, methods.
+## What's next for socialwellness
+I want people to use this, so I'll add some more functionality to keep track of people tasks and tell me their analysis.
---
+++ src/bot.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const {Client,WebhookClient}=require('discord.js')
+const client=new Client();
+const webhookClient = new WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOK_TOKEN,
+  );
+client.login(process.env.DISCORD_BOT_TOKEN)
+
+client.on('ready',()=>{
+    console.log(`${client.user.username} Hello I'm your self Care reminder Bot`);
+})
+const listofTasks={
+
+}
+
+;
+
+const tasks=["water your plants","Go call your old friend and talk to them","Create a drawing of your best friend","Watch Your favourite series","Dance your heart out","sing a song"];
+client.on('message', async (message)=>{
+    //  console.log(message.content);
+    if(message.content==='Hello'){
+    message.reply(`Hello ${message.author.tag}`);
+    message.reply(`Hi ${message.author.tag} These are list of commands that you can use 1.remindContest Contestname ContestLink ContestTime\n 2.funTask \n 3.`)
+    }
+    if(message.content.startsWith('remindContest')){
+        // if( listofTasks[message.author.id]===undefined)
+        // listofTasks[message.author.id]=new Array([message.content]);
+        // else
+        // listofTasks[message.author.id].push(message.content);
+        console.log(message.author.id);
+        console.log(listofTasks[message.author.id]);
+        if( listofTasks[message.author.id]===undefined)
+         listofTasks[message.author.id]=new Array(message.content);
+         else
+        listofTasks[message.author.id].push(message.content);
+        var msg,contestName,contestLink,timeDiff,currentHours,currentMinutes,currentSeconds,startTimeHours,startTimeMinutes,StartTimeSeconds; 
+        msg=message;
+        message=message.content.split(' ');
+        currentHours=new Date().getHours();
+        currentMinutes=new Date().getMinutes();
+        currentSeconds=new Date().getSeconds();
+        startTimeHours=message[3][0]+message[3][1];
+        startTimeMinutes=message[3][3]+message[3][4];
+        StartTimeSeconds=message[3][6]+message[3][7];
+        contestName=message[1];
+        contestLink=message[2];
+        timeDiff=(startTimeHours- currentHours)*60*60+(startTimeMinutes-currentMinutes)*60+(StartTimeSeconds-currentSeconds);
+        console.log(timeDiff);
+        client.setTimeout(function () {
+            // var content = msg.join();
+            msg.reply(`Hello You have your contest ${contestName} now . Please use this link ${contestLink}`)
+         
+            // content = content.replaceAll(',', ' ');
+            // message.reply(content);
+            // console.log('Message sent to ' + message.author.id + ' at ' + Date.now().toString());
+        }, timeDiff*1000);
+    }
+    else if(message.content.startsWith('funTask')){
+        const number=Math.floor(Math.random()*6);
+        message.reply(`${tasks[number]}`);
+    }
+    else if(message.content.startsWith('list')){
+        console.log("hi");
+        message.reply(listofTasks[message.author.id][0]);
+      for(var i=0;i<listofTasks[message.author.id];i++)
+      {
+          message.reply(listofTasks[message.author.id][i]);
+      }
+    }
+})
